310804b283cf53271a238ae59b7310df39cae279
AlexNg <contact@ngjx.org>
2025-07-19 22:27:39 +0800

    feat: Remove old debug prefixes
    Signed-off-by: AlexNg <contact@ngjx.org>

--- pkg/log/log.go
+++ pkg/log/log.go
@@ -20,11 +20,6 @@ const (
 	WARN
 	ERROR
 	QUIET
-
-	debugPrefix = "\x1b[34m[DEBUG]\x1b[0m "
-	infoPrefix  = "\x1b[32m[INFO]\x1b[0m  "
-	warnPrefix  = "\x1b[33m[WARN]\x1b[0m  "
-	errorPrefix = "\x1b[31m[ERROR]\x1b[0m "
 )
 
 var (
