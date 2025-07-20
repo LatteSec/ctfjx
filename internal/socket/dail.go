541de79b36535221539de862c8fe89cd4dcc223a
AlexNg <contact@ngjx.org>
2025-07-20 21:36:46 +0800

    feat: Add dailing with TLS upgrading
    Signed-off-by: AlexNg <contact@ngjx.org>

---
+++ internal/socket/dail.go
@@ -0,0 +1,39 @@
+package socket
+
+import (
+	"fmt"
+	"net"
+	"time"
+
+	"github.com/lattesec/ctfjx/pkg/log"
+)
+
+func DailWithRetry(cfg *ConnConfig) (*Conn, error) {
+	var lastErr error
+	for i := 0; i < cfg.MaxReconnectionAttempts; i++ {
+		conn, err := net.Dial("tcp", cfg.Address)
+		if err != nil {
+			lastErr = err
+			log.Warnf("failed to dial %s: %v\n", cfg.Address, err)
+			time.Sleep(cfg.ReconnectionDelay)
+			continue
+		}
+
+		if cfg.UseTLS {
+			tlsConn, err := WrapTLS(conn, cfg.TLSConfig)
+			if err != nil {
+				lastErr = err
+				conn.Close()
+				log.Warnf("failed to handshake with %s: %v\n", cfg.Address, err)
+				time.Sleep(cfg.ReconnectionDelay)
+				continue
+			}
+
+			return NewConnWithRaw(tlsConn, cfg), nil
+		}
+
+		return NewConnWithRaw(conn, cfg), nil
+	}
+
+	return nil, fmt.Errorf("failed to dial %s after %d attempts: %w", cfg.Address, cfg.MaxReconnectionAttempts, lastErr)
+}
