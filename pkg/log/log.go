7f0f8d4c2cc01b9f29d25c81160341dd0b42d7d6
AlexNg <contact@ngjx.org>
2025-07-22 02:55:37 +0800

    feat: Move logic into Logger with a default global Logger
    Signed-off-by: AlexNg <contact@ngjx.org>

--- pkg/log/log.go
+++ pkg/log/log.go
@@ -1,20 +1,10 @@
 package log
 
 import (
-	"bytes"
-	"compress/gzip"
 	"errors"
 	"fmt"
-	"io"
 	"os"
-	"path/filepath"
-	"strings"
-	"sync"
 	"sync/atomic"
-	"time"
-
-	"github.com/lattesec/ctfjx/internal/helpers/debughelper"
-	"github.com/lattesec/ctfjx/internal/helpers/nopanic"
 )
 
 // Log Level
@@ -35,286 +25,30 @@ const (
 var (
 	levelNames = [6]string{"TRACE", "DEBUG", "INFO", "WARN", "ERROR", "QUIET"}
 
-	logLevel Level = WARN
+	DefaultLogger atomic.Pointer[Logger]
 
-	logFilename string
-	logFileDir  string
-	logfilePtr  atomic.Pointer[os.File]
-
-	stdout  io.Writer = os.Stdout
-	stderr  io.Writer = os.Stderr
-	maxSize int64     = 10 << 20 // 10MB
-
-	logCh     = make(chan string, 1<<20) // The first character of the string will be 0 or 1. 0=stdout, 1=stderr
-	logfileCh = make(chan string, 1<<20) // The first character of the string will be 0 or 1. 0=stdout, 1=stderr
-	closeCh   chan struct{}
-
-	mu     sync.RWMutex
-	muFile sync.RWMutex
-
-	ErrAlreadyInitialized        = errors.New("already initialized")
+	ErrAlreadyStarted            = errors.New("already started")
 	ErrInvalidLogLevel           = errors.New("invalid log level")
 	ErrMissingLogFilename        = errors.New("missing log filename")
 	ErrNoLogFileConfigured       = errors.New("no log file configured")
 	ErrFoundDirWhenExpectingFile = errors.New("found directory when expecting file")
 )
 
-func GetLevel() Level {
-	mu.RLock()
-	defer mu.RUnlock()
-	return logLevel
+func init() {
+	DefaultLogger.Store(NewLogger("default"))
 }
 
-func Init(dir, filename string, lvl Level) error {
-	mu.Lock()
-	if lvl < TRACE || lvl > QUIET {
-		return ErrInvalidLogLevel
-	}
-	logLevel = lvl
-
-	if closeCh != nil {
-		return ErrAlreadyInitialized
-	}
-	closeCh = make(chan struct{})
-
-	logFileDir = filepath.Clean(dir)
-	logFilename = filepath.Clean(strings.TrimSuffix(filepath.Base(filename), ".log"))
-
-	if logFileDir != "." && logFilename == "." {
-		return ErrMissingLogFilename
-	}
-	if logFilename != "." {
-		logFilename += ".log"
-	}
-	mu.Unlock()
-
-	if logFilename != "." {
-		go nopanic.NoPanicReRunVoid("log file writer", fileWriter)
-		go nopanic.NoPanicReRunVoid("log file rotater", logRotater)
-	}
-
-	go nopanic.NoPanicReRunVoid("log I/O writer", logWriter)
-
-	return nil
-}
-
-func Close() {
-	mu.Lock()
-	defer mu.Unlock()
-	close(closeCh)
-	closeLogFile()
-}
-
-func closeLogFile() {
-	muFile.Lock()
-	defer muFile.Unlock()
-	close(logfileCh)
-	ptr := logfilePtr.Load()
-	if ptr != nil {
-		_ = ptr.Close()
-		logfilePtr.Store(nil)
-	}
-}
-
-func logWriter() {
-	for {
-		select {
-		case line := <-logCh:
-			if line[0] == '0' {
-				fmt.Fprint(stdout, line[1:])
-			} else {
-				fmt.Fprint(stderr, line[1:])
-			}
-		case <-closeCh:
-			return
-		}
-	}
-}
-
-func fileWriter() {
-	muFile.Lock()
-	if logfileCh == nil {
-		logfileCh = make(chan string, 1<<20)
-	}
-	muFile.Unlock()
-
-	logfile, err := ensureLogFile()
-	if err != nil {
-		Errorln("failed to open log file:", err)
-		return
-	}
-	logfilePtr.Store(logfile)
-
-	for {
-		select {
-		case line := <-logfileCh:
-			muFile.Lock()
-			_, err := logfilePtr.Load().WriteString(line[1:])
-			muFile.Unlock()
-			if err != nil {
-				Errorln("failed to write to log file:", err)
-				return
-			}
-		case <-closeCh:
-			return
-		}
-	}
-}
-
-func logRotater() {
-	ticker := time.NewTicker(time.Minute)
-	defer ticker.Stop()
-
-	for {
-		select {
-		case <-ticker.C:
-			mu.RLock()
-			logPath := filepath.Join(logFileDir, logFilename)
-			mu.RUnlock()
-
-			info, err := os.Stat(logPath)
-			if err != nil {
-				if os.IsNotExist(err) {
-					if _, err := ensureLogFile(); err != nil {
-						Errorln("failed to recreate missing log file, killing rotation:", err)
-						return
-					}
-					continue
-				}
-				Errorln("failed to stat log file:", err)
-				return
-			}
-
-			if info.Size() <= maxSize {
-				continue
-			}
-
-			muFile.Lock()
-
-			rotatedName := fmt.Sprintf("%s-%s.gz", logFilename, time.Now().UTC().Format("2006-01-02_15-04-05"))
-			rotatedPath := filepath.Join(logFileDir, rotatedName)
-
-			original, err := os.Open(filepath.Clean(logPath))
-			if err != nil {
-				muFile.Unlock()
-				Errorln("failed to open log for rotation:", err)
-				continue
-			}
-
-			var buf bytes.Buffer
-			gz := gzip.NewWriter(&buf)
-			_, err = io.Copy(gz, original)
-			_ = original.Close()
-			_ = gz.Close()
-			if err != nil {
-				muFile.Unlock()
-				Errorln("failed to compress rotated log:", err)
-				continue
-			}
-
-			if err := os.WriteFile(rotatedPath, buf.Bytes(), 0o600); err != nil {
-				muFile.Unlock()
-				Errorln("failed to write rotated log file:", err)
-				continue
-			}
-
-			if err := os.Truncate(logPath, 0); err != nil {
-				Errorln("failed to truncate original log after rotation:", err)
-			}
-
-			muFile.Unlock()
-
-		case <-closeCh:
-			return
-		}
-	}
-}
-
-func ensureLogDir() error {
-	if logFileDir == "." {
-		return nil
-	}
-
-	return os.MkdirAll(filepath.Clean(logFileDir), 0o700)
-}
-
-func ensureLogFile() (*os.File, error) {
-	mu.RLock()
-	defer mu.RUnlock()
-
-	if logFilename == "." {
-		return nil, ErrNoLogFileConfigured
-	}
-	if err := ensureLogDir(); err != nil {
-		return nil, err
-	}
-
-	logfileLocation := filepath.Join(logFileDir, logFilename)
-	if logfileLocation == "." {
-		return nil, ErrNoLogFileConfigured
-	}
-
-	stat, err := os.Stat(logfileLocation)
-	if err != nil && !os.IsNotExist(err) {
-		return nil, err
-	}
-	if os.IsNotExist(err) {
-		return openLogFile(logfileLocation)
-	}
-
-	if stat.IsDir() {
-		return nil, ErrFoundDirWhenExpectingFile
-	}
-
-	return openLogFile(logfileLocation)
-}
-
-func openLogFile(path string) (*os.File, error) {
-	return os.OpenFile(filepath.Clean(path), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0o600)
-}
-
-func log(lvl Level, msg string) {
-	mu.RLock()
-	if lvl < logLevel {
+func Log(msg *LogMessage) {
+	logger := DefaultLogger.Load()
+	if logger == nil {
 		return
 	}
 
-	ts := time.Now().UTC().Format(time.RFC3339Nano)
-	lines := []string{}
+	logger.Log(msg)
+}
 
-	if logLevel == TRACE && (lvl == TRACE || lvl == ERROR) {
-		lines = append(lines,
-			fmt.Sprintf("%s [TRACE] %s", ts, debughelper.TraceCaller()),
-			fmt.Sprintf("%s [TRACE] %s", ts, debughelper.TraceStack()),
-		)
-	}
-
-	lines = append(lines, fmt.Sprintf("%s [%s] %s", ts, levelNames[lvl], msg))
-	shouldWriteToIO := logLevel < QUIET
-	mu.RUnlock()
-
-	full := strings.Join(lines, "\n")
-	if lvl >= WARN {
-		full = "1" + full
-	} else {
-		full = "0" + full
-	}
-
-	if shouldWriteToIO {
-		select {
-		case logCh <- full:
-		case <-closeCh:
-		default: // drop logs when buffer is full
-		}
-	}
-
-	if logfilePtr.Load() != nil {
-		select {
-		case logfileCh <- full:
-		case <-closeCh:
-		default: // drop logs when buffer is full
-		}
-	}
+func log(level Level, msg string) {
+	Log(NewLogMessage(level, msg))
 }
 
 func Debug(v ...any) { log(DEBUG, fmt.Sprint(v...)) }
